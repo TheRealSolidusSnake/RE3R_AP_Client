6a0f5adf2af8442b040bba5c948636dc84e3a8d1
Solidus Snake <63137482+TheRealSolidusSnake@users.noreply.github.com>
2025-11-08 10:56:35 -0500

    Revert the revert
    Just changed the call on DrawSelf() to a pcall instead to ignore the error it throws on scene change/death, seems to be fine.

--- reframework/autorun/randomizer/Scene.lua
+++ reframework/autorun/randomizer/Scene.lua
@@ -2,31 +2,54 @@ local Scene = {}
 
 Scene.sceneObject = nil
 Scene.mainFlowManager = nil
+Scene.interactManager = nil
+Scene.guiItemBox = nil
 
 function Scene.getSceneObject()
     if Scene.sceneObject ~= nil then
         return Scene.sceneObject
     end
 
-    Scene.sceneObject = sdk.call_native_func(sdk.get_native_singleton("via.SceneManager"), sdk.find_type_definition("via.SceneManager"), "get_CurrentScene()")
+    local mgr = sdk.get_native_singleton("via.SceneManager")
+    if mgr == nil then
+        return nil
+    end
+
+    Scene.sceneObject = sdk.call_native_func(
+        mgr,
+        sdk.find_type_definition("via.SceneManager"),
+        "get_CurrentScene()"
+    )
 
     return Scene.sceneObject
 end
 
-function Scene.getGameMaster()
-    -- local gameMaster = Scene.getSceneObject():findGameObject("30_GameMaster")
-    local masters = Scene.getSceneObject():findGameObjectsWithTag("Masters")
-    local gameMaster = nil
+function Scene.getMasterObject(objectName)
+    local scene = Scene.getSceneObject()
+    if not scene then return nil end
 
-    for k, master in pairs(masters) do
-        if master:get_Name() == "30_GameMaster" then
-            gameMaster = master
+    local masters = scene:findGameObjectsWithTag("Masters")
+    if not masters then return nil end
 
-            break
+    for _, master in pairs(masters) do
+        if master and master:get_Name() == objectName then
+            return master
         end
     end
 
-    return gameMaster
+    return nil
+end
+
+function Scene.getGameMaster()
+    return Scene.getMasterObject("30_GameMaster")
+end
+
+function Scene.getGimmickMaster()
+    return Scene.getMasterObject("70_GimmickMaster")
+end
+
+function Scene.getUIMaster()
+    return Scene.getMasterObject("UIMaster")
 end
 
 function Scene.getMainFlowManager()
@@ -35,21 +58,48 @@ function Scene.getMainFlowManager()
     end
 
     local gameMaster = Scene.getGameMaster()
+    if not gameMaster then return nil end
 
-    Scene.mainFlowManager = gameMaster:call("getComponent(System.Type)", sdk.typeof(sdk.game_namespace("gamemastering.MainFlowManager")))
+    Scene.mainFlowManager = gameMaster:call(
+        "getComponent(System.Type)",
+        sdk.typeof(sdk.game_namespace("gamemastering.MainFlowManager"))
+    )
 
     return Scene.mainFlowManager
 end
 
+function Scene.getInteractManager()
+    if Scene.interactManager ~= nil then
+        return Scene.interactManager
+    end
+
+    local gimmickMaster = Scene.getGimmickMaster()
+    if not gimmickMaster then return nil end
+
+    Scene.interactManager = gimmickMaster:call(
+        "getComponent(System.Type)",
+        sdk.typeof(sdk.game_namespace("gimmick.action.InteractManager"))
+    )
+
+    return Scene.interactManager
+end
+
 function Scene.getSurvivorType()
     local gameMaster = Scene.getGameMaster()
-    local survivorManager = gameMaster:call("getComponent(System.Type)", sdk.typeof(sdk.game_namespace("SurvivorManager")))
-    local survivors = survivorManager:get_field("ExistSurvivorInfoList")
+    if not gameMaster then return -1 end
 
-    for _, survivor in pairs(survivors:get_field("mItems")) do
+    local survivorManager = gameMaster:call(
+        "getComponent(System.Type)",
+        sdk.typeof(sdk.game_namespace("SurvivorManager"))
+    )
+    if not survivorManager then return -1 end
+
+    local survivors = survivorManager:get_field("ExistSurvivorInfoList")
+    if not survivors then return -1 end
+
+    for _, survivor in pairs(survivors:get_field("mItems") or {}) do
         if survivor then
             local isActive = survivor:get_field("<IsActivePlayer>k__BackingField")
-
             if isActive then
                 return survivor:get_field("<SurvivorType>k__BackingField")
             end
@@ -64,31 +114,58 @@ function Scene.getGUIItemBox()
         return Scene.guiItemBox
     end
 
-    return Scene.getSceneObject():findGameObject("GUI_ItemBox")
-end
+    local scene = Scene.getSceneObject()
+    if not scene then return nil end
 
-function Scene.isTitleScreen()
-    return Scene.getMainFlowManager():get_IsInTitle()
-end
-
-function Scene.isInGame()
-    return Scene.getMainFlowManager():get_IsInGame()
-end
-
-function Scene.isInPause()
-    return Scene.getMainFlowManager():get_IsInPause()
-end
-
-function Scene.isInGameOver()
-    return Scene.getMainFlowManager():get_IsInGameOver()
-end
-
-function Scene.goToGameOver()
-    return Scene.getMainFlowManager():call("goGameOverSimple", nil)
+    Scene.guiItemBox = scene:findGameObject("GUI_ItemBox")
+    return Scene.guiItemBox
 end
 
 function Scene.isUsingItemBox()
-    return Scene.getGUIItemBox():get_DrawSelf() -- is the ItemBox GUI "drawn"?
+    local guiBox = Scene.getGUIItemBox()
+    if not guiBox then
+        return false
+    end
+
+    -- get_DrawSelf() can throw an error when RE3 reloads a scene or on death, so pcall that ish since that works just fine
+    local success, result = pcall(function()
+        return guiBox:get_DrawSelf()
+    end)
+
+    if not success then
+        -- invalidate cached reference so the next lookup gets a fresh object
+        Scene.guiItemBox = nil
+        return false
+    end
+
+    return result
+end
+
+
+function Scene.isTitleScreen()
+    local mfm = Scene.getMainFlowManager()
+    return mfm and mfm:get_IsInTitle() or false
+end
+
+function Scene.isInGame()
+    local mfm = Scene.getMainFlowManager()
+    return mfm and mfm:get_IsInGame() or false
+end
+
+function Scene.isInPause()
+    local mfm = Scene.getMainFlowManager()
+    return mfm and mfm:get_IsInPause() or false
+end
+
+function Scene.isInGameOver()
+    local mfm = Scene.getMainFlowManager()
+    return mfm and mfm:get_IsInGameOver() or false
+end
+
+function Scene.goToGameOver()
+    local mfm = Scene.getMainFlowManager()
+    if not mfm then return false end
+    return mfm:call("goGameOverSimple", nil)
 end
 
 function Scene.isCharacterJill()
@@ -100,19 +177,23 @@ function Scene.isCharacterCarlos()
 end
 
 function Scene.getCurrentLocation()
-    return Scene.getMainFlowManager():get_LoadLocation()
+    local mfm = Scene.getMainFlowManager()
+    return mfm and mfm:get_LoadLocation() or nil
 end
 
 function Scene.getCurrentArea()
-    return Scene.getMainFlowManager():get_LoadArea()
+    local mfm = Scene.getMainFlowManager()
+    return mfm and mfm:get_LoadArea() or nil
 end
 
 function Scene.getGameGUID()
-    return Scene.getMainFlowManager():get_GameGUID()
+    local mfm = Scene.getMainFlowManager()
+    return mfm and mfm:get_GameGUID() or nil
 end
 
 function Scene.getSaveGUID()
-    return Scene.getMainFlowManager():get_SaveGUID()
+    local mfm = Scene.getMainFlowManager()
+    return mfm and mfm:get_SaveGUID() or nil
 end
 
 return Scene
